92b2fdd35fe71c08f2fe15afc0328d6aad3fea3a
Mehdi Stoury <mehdistoury@gmail.com>
2023-05-03 17:53:48 +0100

    add upload multiple file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Form from "./components/Form";
 import List from "./components/List";
+import FileUploadMultiple from "./components/FileUploadMultiple";
 
 function App() {
   return (
-    <div className="App">
-      <List />
-    </div>
+    <>
+      <div className="App">
+        <List />
+        <br />
+        <FileUploadMultiple />
+      </div>
+    </>
   );
 }
 
